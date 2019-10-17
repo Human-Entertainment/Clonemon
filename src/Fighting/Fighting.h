87620d00edc6de0eab5d52f8ac08bda4335b67d0
Bastian Inuk Christensen <bastian@inuk.blog>
2019-10-17 08:24:00 +0200

    A little more prep for the fighting mechanic

---
+++ src/Fighting/Fighting.h
@@ -0,0 +1,17 @@
+//
+//  Fighting.h
+//  Clonémon
+//
+//  Created by Bastian Inuk Christensen on 16/10/2019.
+//  Copyright © 2019 Inuk Entertainment. All rights reserved.
+//
+
+#ifndef Fighting_h
+#define Fighting_h
+
+//#include "Player.h"
+#include "Monster.h"
+
+void attack(Attack attack, Monster attacker, Monster attackee);
+
+#endif /* Fighting_h */
---
+++ src/Fighting/fighting.c
@@ -0,0 +1,15 @@
+#include "Fighting.h"
+
+void attackNow(const Attack* attack, const Monster* attacker, Monster* attackee){
+    switch(attackee->type){
+    case rock:
+            
+            break;
+    case scissor:
+            
+            break;
+    case paper:
+            
+            break;
+    }
+}
